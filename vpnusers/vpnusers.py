4d08dd0600af23fdf239bd7b9b46d85f2473809f
Nikolay <rusatch@gmail.com>
2024-03-14 11:00:21 +0500

    added simple script to get connected ovpn-users

---
+++ vpnusers/vpnusers.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+import telnetlib
+
+HOST = "localhost"
+PORT = "7505"
+
+telnet = telnetlib.Telnet(HOST,PORT, 5)
+telnet.write("status\n".encode("ascii"))
+output = telnet.read_until("END\n".encode('utf-8'), 1).decode('ascii').split("\n")
+telnet.close()
+
+print('Common Name'.ljust(15), 'Real Address'.ljust(20), 'Virtual Address'.ljust(15), 'Bytes Received'.ljust(15), 'Bytes Sent'.ljust(15), 'Connected Since'.ljust(15))
+for string in output:
+    if 'CLIENT_LIST' in string and len(string.split(",")) == 13:
+        print(string.split(",")[1].ljust(15), # Common Name
+              string.split(",")[2].split(':')[0].ljust(20), # Real Address
+              string.split(",")[3].ljust(15), # Virtual Address
+              string.split(",")[5].ljust(15), # Bytes Received
+              string.split(",")[6].ljust(15), # Bytes Sent
+              string.split(",")[7].ljust(15)  # Connected Since
+              )
